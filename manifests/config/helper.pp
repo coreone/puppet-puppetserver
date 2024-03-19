ee2bc6059b3f6561b0e2a437e82101baa005c5a3
Andrew Teixeira <teixeira@broadinstitute.org>
2024-03-19 17:27:19 -0400

    Update to be compatible with Puppet 7

--- manifests/config/helper.pp
+++ manifests/config/helper.pp
@@ -1,19 +1,23 @@
 define puppetserver::config::helper (
-  $type,
-  $setting,
-  $setting_type = undef,
+  String $type,
+  String $setting,
+  Optional[String] $setting_type = undef,
 ) {
   $split_name = split($name, '/')
   $section = $split_name[0]
   $key = $split_name[1]
 
-  if has_key($::puppetserver::config, $section) {
-    if has_key($::puppetserver::config[$section], $key) {
-      ensure_resource("puppetserver::config::${type}", $setting, {
+  if $section in $puppetserver::config {
+    if $key in $puppetserver::config[$section] {
+      ensure_resource(
+        "puppetserver::config::${type}",
+        $setting,
+        {
           ensure       => 'present',
-          value        => $::puppetserver::config[$section][$key],
+          value        => $puppetserver::config[$section][$key],
           setting_type => $setting_type,
-        })
+        }
+      )
     }
   }
 }
